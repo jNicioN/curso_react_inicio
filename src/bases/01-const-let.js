3166892c70f62825289dc101fba554c38437f8fc
Jonathan Nicio <jonathan.nicio@banregio.com>
2021-10-15 14:48:05 -0500

    feat: Crea JS para entendimiento de constantes

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+const nombre = 'Jonathan';
+
+const apellido = 'Nicio';
+
+let valorDado = 5;
+
+valorDado = 4;
+
+console.log( nombre, apellido, valorDado);
+
+if (true){
+	let valorDado = 6;
+
+	console.log(valorDado);
+}
+
+console.log(valorDado);

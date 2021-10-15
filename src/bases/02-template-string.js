ec46f91dfcb884210addcfac77e1bc72be361275
Jonathan Nicio <jonathan.nicio@banregio.com>
2021-10-15 14:48:34 -0500

    feat: Crea JS para entendimiento de cadenas

---
+++ src/bases/02-template-string.js
@@ -0,0 +1,14 @@
+const nombre = 'Jonathan';
+const apellido = 'Nicio';
+
+//const nombreCompleto = nombre + ' ' + apellido;
+
+const nombreCompleto = ` ${nombre} ${apellido} `;
+
+console.log(nombreCompleto);
+
+function getSaludo(nombre){
+	return 'Hola ' + nombre;
+}
+
+console.log( `Este es un texto: ${getSaludo('Jonathan')}`);
